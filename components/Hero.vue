0cebda1cf83de0dcb4734f5b690e76daef981fd2
Widdy LOUIS <louisws@outlook.fr>
2023-07-02 21:57:12 +0200

    Création du composant - Hero

---
+++ components/Hero.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+<template>
+  <div class="mt-40 w-full">
+    <h1 class="font-medium mt-20 w-full text-4xl text-dark-900 lg:text-7xl lg:w-3/4 dark-text-gray-50">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    </h1>
+    <div class="py-40">
+      
+    </div>
+  </div>
+</template>
