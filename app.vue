7980edd00f32c4e411d11d59a9124b6aeb7ac9dd
Widdy LOUIS <louisws@outlook.fr>
2023-07-02 22:43:52 +0200

    Update app.vue

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <main class="h-auto w-full relative dark:bg-dark-900">
     <Header/>
     <div class="mx-auto w-full min-h-screen px-10 lg:max-w-screen-xl lg:px-0">
-      <!-- <NuxtPage/> -->
+      <nuxtPage/>
     </div>
   </main>
 </template>
