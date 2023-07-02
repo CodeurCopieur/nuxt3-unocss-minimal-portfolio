283fbae89c866e544d2b943646002c7aa4f06a65
Widdy LOUIS <louisws@outlook.fr>
2023-07-02 14:46:09 +0200

    configuration du projet

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@unocss/nuxt', '@nuxtjs/color-mode'],
+  unocss: {
+    uno: true,
+    preflight: true
+  },
+  colorMode: {
+    preference: 'system',
+    classSuffix: '',
+  },
   devtools: { enabled: true }
 })
