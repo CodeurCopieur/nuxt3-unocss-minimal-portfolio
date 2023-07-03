e48b9c3cdb3919e4b684a83a496f394d70dae272
Widdy LOUIS <louisws@outlook.fr>
2023-07-03 23:49:34 +0200

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="h-auto w-full relative dark:bg-dark-900">
     <Header/>
-    <div class="mx-auto w-full min-h-screen px-10 lg:max-w-screen-xl lg:px-0">
+    <div class="mx-auto w-full min-h-screen px-6 lg:max-w-screen-xl lg:px-0">
       <nuxtPage/>
     </div>
     <Footer/>
