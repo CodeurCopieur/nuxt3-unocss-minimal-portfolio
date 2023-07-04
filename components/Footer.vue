41f528b13ef49e92164acc2d3292a106b8d1637b
Widdy LOUIS <louisws@outlook.fr>
2023-07-04 20:36:19 +0200

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -5,10 +5,16 @@
 <template>
   <footer class="py-8">
     <div class="flex mx-auto w-full gap-4 items-center justify-center md:w-1/2">
-      <a href="https://github.com/CodeurCopieur" class="text-gray-400 inline-block hover:text-gray-900">
+      <a 
+        href="https://github.com/CodeurCopieur" 
+        class="text-gray-400 inline-block"
+        :class="{'hover:text-dark-900' :$colorMode.preference !== 'dark' }">
         Github
       </a>
-      <a href="https://twitter.com/CodeurCopieur" class="text-gray-400 inline-block hover:text-gray-900">
+      <a 
+        href="https://twitter.com/CodeurCopieur" 
+        class="text-gray-400 inline-block"
+        :class="{'hover:text-dark-900' :$colorMode.preference !== 'dark' }">
         Twitter
       </a>
     </div>
