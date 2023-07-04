f84c7812c55f5fb91ce81ffff6e3454c3a4e9975
Widdy LOUIS <louisws@outlook.fr>
2023-07-04 20:36:12 +0200

    Creating the About view

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
-    <h1>about</h1>
+  <div class="flex flex-col min-h-screen mt-40 w-full gap-10 items-center lg:flex-row lg:mt-0 lg:px-10">
+    <div class="w-full lg:w-1/2">
+      <p class="font-light text-2xl text-gray-900 dark-text-gray-500">
+        Hello, I'm Wid
+        <br/><br/>
+        I am a developer based in Paris, France
+        <br/><br/>
+        Passionate about fitness, manga, high-tech, web development. I'm a movie buff
+        <br/><br/>
+        You can contact me at <span class="text-gray-900">louisws@outlook.fr</span>
+      </p>
+    </div>
+    <div class="rounded-xl w-full lg:w-1/2">
+      <img src="https://plus.unsplash.com/premium_photo-1672784163077-2d500d816a0a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MzUyfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=600&q=60" alt="" class="object-cover object-right rounded-2xl h-160 shadow-xl w-full shadow-blue-100">
+    </div>
   </div>
 </template>
