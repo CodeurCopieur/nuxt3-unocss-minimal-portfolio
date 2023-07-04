885b43bc023899623a3add35a737dda76a190adb
Widdy LOUIS <louisws@outlook.fr>
2023-07-04 20:36:15 +0200

    Update Hero.vue

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="pt-40 w-full">
-    <h1 class="font-medium mt-20 w-full text-4xl text-dark-900 pl-5 xs:pl-0 lg:text-7xl lg:w-3/4 dark-text-gray-500">
+    <h1 class="font-medium mt-20 w-full text-4xl text-dark-900 pl-5 xs:pl-0 lg:text-4xl lg:w-3/4 dark-text-gray-500">
       Hi, I'm a front end dev.<br> my specialty is vue3, nuxt3, gsap, tailwindcss
     </h1>
     <div class="py-40">
